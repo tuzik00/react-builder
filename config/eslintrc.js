bb33f4ce869d6f7ab5406020fcdc96a9128d2089
tuzik00 <geka00@gmail.com>
2019-04-20 00:30:12 +0300

    Update eslintrc.js
    update eslint

--- config/eslintrc.js
+++ config/eslintrc.js
@@ -20,6 +20,6 @@ module.exports = {
         'global-require': 0,
         'eslint linebreak-style': [0, 'error', 'windows'],
         'indent': ['error', 4, {SwitchCase: 1}],
-        'no-underscore-dangle': ['error', { 'allow': ['__REDUX_DEVTOOLS_EXTENSION__'] }],
+        'no-underscore-dangle': ['error', { 'allow': ['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] }],
     },
 };
